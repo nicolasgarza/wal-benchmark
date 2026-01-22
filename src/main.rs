d45b9fc7979ec73807188c59e13ad42631b061ad
nicolasgarza <nicolas.garza@gmail.com>
2026-01-22 02:32:29 +0000

    cargo fmt

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 #![allow(dead_code)]
 use std::fs::{self, File, OpenOptions};
+use std::io::{BufWriter, Error, Write};
 use std::os::unix::fs::OpenOptionsExt;
-use std::io::{Error, Write, BufWriter};
 use std::sync::{Arc, Barrier, Mutex};
 use std::thread;
 use std::time::Instant;
@@ -15,8 +15,8 @@ const A1000: [u8; 1000] = [b'a'; 1000];
 const ABLOCK: [u8; BLOCK_SIZE] = [b'a'; BLOCK_SIZE];
 
 struct ExperimentConfig {
-    threads: Vec<u64>, 
-    to_write: Vec<&'static [u8]>, 
+    threads: Vec<u64>,
+    to_write: Vec<&'static [u8]>,
     iterations: Vec<u64>,
     experiment: fn(&[u8], u64, u64) -> Result<f64, Error>,
 }
@@ -32,22 +32,25 @@ fn main() {
         threads: vec![1, 4, 16, 64],
         to_write: vec![&A100, &A100, &A100, &A100],
         iterations: vec![10_000, 10_000, 10_000, 10_000],
-        experiment: thread_work_unoptimized
+        experiment: thread_work_unoptimized,
     };
 
     runner(config, cores);
 
     println!("---------------");
     //
-    
+
     //
     println!("---------------");
-    println!("testing different thread counts - BATCHING, size = {}", BATCH_SIZE);
+    println!(
+        "testing different thread counts - BATCHING, size = {}",
+        BATCH_SIZE
+    );
     let config = ExperimentConfig {
         threads: vec![1, 4, 16, 64],
         to_write: vec![&A100, &A100, &A100, &A100],
         iterations: vec![10_000, 10_000, 10_000, 10_000],
-        experiment: thread_work_batching
+        experiment: thread_work_batching,
     };
 
     runner(config, cores);
@@ -62,12 +65,12 @@ fn main() {
         threads: vec![1, 4, 16, 64],
         to_write: vec![&ABLOCK, &ABLOCK, &ABLOCK, &ABLOCK],
         iterations: vec![10_000, 10_000, 10_000, 10_000],
-        experiment: thread_work_unoptimized
+        experiment: thread_work_unoptimized,
     };
 
     runner(config, cores);
 
-    // 
+    //
     println!("---------------");
     println!("testing different thread counts, writing by block size - BATCHING");
 
@@ -75,11 +78,11 @@ fn main() {
         threads: vec![1, 4, 16, 64],
         to_write: vec![&ABLOCK, &ABLOCK, &ABLOCK, &ABLOCK],
         iterations: vec![10_000, 10_000, 10_000, 10_000],
-        experiment: thread_work_batching
+        experiment: thread_work_batching,
     };
-    
+
     runner(config, cores);
-    
+
     /*
     // TESTING KEY LEN
     println!("---------------");
@@ -94,9 +97,13 @@ fn main() {
     */
 }
 
-
 fn runner(config: ExperimentConfig, cores: &Vec<core_affinity::CoreId>) {
-    for ((&n_threads, &writing), &iteration) in config.threads.iter().zip(&config.to_write).zip(&config.iterations) {
+    for ((&n_threads, &writing), &iteration) in config
+        .threads
+        .iter()
+        .zip(&config.to_write)
+        .zip(&config.iterations)
+    {
         let start = Instant::now();
         let barrier = Barrier::new(n_threads as usize);
 
@@ -126,17 +133,24 @@ fn runner(config: ExperimentConfig, cores: &Vec<core_affinity::CoreId>) {
 
             handles
                 .into_iter()
-                .map(|h| h.join().expect("thread panicked").expect("experiment failed"))
+                .map(|h| {
+                    h.join()
+                        .expect("thread panicked")
+                        .expect("experiment failed")
+                })
                 .collect()
         });
 
         let elapsed = start.elapsed().as_secs_f64();
         print_aggregates(times, n_threads, iteration, elapsed);
-    } 
+    }
 }
 
 fn thread_work_unoptimized(to_write: &[u8], iterations: u64, i: u64) -> Result<f64, Error> {
-    let mut file = OpenOptions::new().create(true).append(true).open(format!("wal_{}.log", i))?;
+    let mut file = OpenOptions::new()
+        .create(true)
+        .append(true)
+        .open(format!("wal_{}.log", i))?;
     let start = Instant::now();
     for _ in 0..iterations {
         file.write_all(to_write)?;
@@ -150,7 +164,10 @@ fn thread_work_unoptimized(to_write: &[u8], iterations: u64, i: u64) -> Result<f
 }
 
 fn thread_work_batching(to_write: &[u8], iterations: u64, i: u64) -> Result<f64, Error> {
-    let mut file = OpenOptions::new().create(true).append(true).open(format!("wal_{}.log", i))?;
+    let mut file = OpenOptions::new()
+        .create(true)
+        .append(true)
+        .open(format!("wal_{}.log", i))?;
     let start = Instant::now();
     for iter_num in 0..iterations {
         file.write_all(to_write)?;
@@ -168,10 +185,10 @@ fn thread_work_batching(to_write: &[u8], iterations: u64, i: u64) -> Result<f64,
 // not working - getting invalid argument - probl need to align writes or something
 fn thread_work_flags(to_write: &[u8], iterations: u64, i: u64) -> Result<f64, Error> {
     let mut file = OpenOptions::new()
-                    .create(true)
-                    .append(true)
-                    .custom_flags(libc::O_DIRECT | libc::O_SYNC)
-                    .open(format!("wal_{}.log", i))?;
+        .create(true)
+        .append(true)
+        .custom_flags(libc::O_DIRECT | libc::O_SYNC)
+        .open(format!("wal_{}.log", i))?;
 
     let start = Instant::now();
     for _ in 0..iterations {
@@ -184,7 +201,10 @@ fn thread_work_flags(to_write: &[u8], iterations: u64, i: u64) -> Result<f64, Er
 
 // no benefit - probably not using bufwriter correctly. not sure if using this will yield anything major
 fn thread_work_bufwriter(to_write: &[u8], iterations: u64, i: u64) -> Result<f64, Error> {
-    let file = OpenOptions::new().create(true).append(true).open(format!("wal_{}.log", i))?;
+    let file = OpenOptions::new()
+        .create(true)
+        .append(true)
+        .open(format!("wal_{}.log", i))?;
     let mut writer = BufWriter::new(file);
     let start = Instant::now();
     for iter_num in 0..iterations {
@@ -210,9 +230,6 @@ fn print_aggregates(times: Vec<f64>, threads: u64, iterations: u64, elapsed: f64
 
     println!(
         "threads: {:>} | writes to complete: {:>} | average time for thread to complete: {:>6.3} | writes per second {:>6.3}",
-        threads,
-        iterations,
-        avg_thread_time,
-        writes_per_sec,
+        threads, iterations, avg_thread_time, writes_per_sec,
     );
 }
